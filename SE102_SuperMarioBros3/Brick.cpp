11672ea40d9848d0db8436a1f5db256323cf23f5
pahopu <20520278@gm.uit.edu.vn>
2023-05-31 15:55:03 +0700

    Add question brick, empty brick. gold brick

--- SE102_SuperMarioBros3/Brick.cpp
+++ SE102_SuperMarioBros3/Brick.cpp
@@ -1,9 +1,33 @@
 #include "Brick.h"
 
+
+void CBrick::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if (time_start == -1)
+		return;
+
+	if (type == BRICK_TYPE_EMPTY) {
+		vy += ay * dt;
+		y += vy * dt;
+
+		if (y > old_y) {
+			y = old_y;
+			time_start = -1;
+			vy = ay = 0;
+		}
+	}
+}
+
 void CBrick::Render()
 {
+	int aniId = ID_ANI_GOLD_BRICK;
+	if (type == BRICK_TYPE_QUESTION)
+		aniId = ID_ANI_QUESTION_BRICK;
+	else if (type == BRICK_TYPE_EMPTY)
+		aniId = ID_ANI_EMPTY_BRICK;
+
 	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_BRICK)->Render(x, y);
+	animations->Get(aniId)->Render(x, y);
 	//RenderBoundingBox();
 }
 
@@ -13,4 +37,21 @@ void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
 	t = y - BRICK_BBOX_HEIGHT/2;
 	r = l + BRICK_BBOX_WIDTH;
 	b = t + BRICK_BBOX_HEIGHT;
+}
+
+void CBrick::SetType(int type)
+{
+	this->type = type;
+	switch (type) {
+	case BRICK_TYPE_GOLD:
+		break;
+	case BRICK_TYPE_QUESTION:
+		break;
+	case BRICK_TYPE_EMPTY:
+		time_start = GetTickCount64();
+		old_y = y;
+		ay = BRICK_GRAVITY;
+		vy = -BRICK_TRANSFORM_DEFLECT;
+		break;
+	}
 }
--- SE102_SuperMarioBros3/Brick.h
+++ SE102_SuperMarioBros3/Brick.h
@@ -4,15 +4,48 @@
 #include "Animation.h"
 #include "Animations.h"
 
-#define ID_ANI_BRICK 10000
-#define BRICK_WIDTH 16
-#define BRICK_BBOX_WIDTH 16
-#define BRICK_BBOX_HEIGHT 16
+#define BRICK_GRAVITY				0.002f
+#define BRICK_TRANSFORM_DEFLECT		0.2f
+
+#define BRICK_BREAK_TIMEOUT			500
+
+#define BRICK_BBOX_WIDTH			15
+#define BRICK_BBOX_HEIGHT			15
+
+#define BRICK_TYPE_GOLD				1
+#define BRICK_TYPE_QUESTION			2
+#define BRICK_TYPE_EMPTY			3
+
+#define ID_ANI_GOLD_BRICK			10000
+#define ID_ANI_QUESTION_BRICK		10001
+#define ID_ANI_EMPTY_BRICK			10002
+
 
 class CBrick : public CGameObject {
+protected:
+	float old_y;
+
+	float ax;
+	float ay;
+
+	int type;
+	ULONGLONG time_start;
+
+	virtual void OnNoCollision(DWORD dt) {}
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e) {};
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
+
 public:
-	CBrick(float x, float y) : CGameObject(x, y) {}
+	CBrick(float x, float y, int type) : CGameObject(x, y) {
+		SetType(type);
+		time_start = -1;
+		old_y = ax = ay = vx = vy = 0;
+	}
+
 	void Render();
 	void Update(DWORD dt) {}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
+
+	int GetType() { return type; }
+	void SetType(int type);
 };
