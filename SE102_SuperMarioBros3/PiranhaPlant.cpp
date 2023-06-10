92762fabb986d876bb69a26d2049c9d7a70db671
pahopu <20520278@gm.uit.edu.vn>
2023-06-10 10:05:33 +0700

    Add up down attack idle basic for piranha plant

--- SE102_SuperMarioBros3/PiranhaPlant.cpp
+++ SE102_SuperMarioBros3/PiranhaPlant.cpp
@@ -1,4 +1,8 @@
 #include "PiranhaPlant.h"
+#include "Mario.h"
+#include "PlayScene.h"
+#include "Game.h"
+#include "debug.h"
 
 void CPiranhaPlant::GetBoundingBox(float& left, float& top, float& right, float& bottom) {
 	if (type == PIRANHA_TYPE_RED_FIRE) {
@@ -26,6 +30,47 @@ void CPiranhaPlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 	vy += ay * dt;
 	y += vy * dt;
 
+	switch (state) {
+	case PIRANHA_STATE_UP:
+		switch (type) {
+		case PIRANHA_TYPE_GREEN:
+		case PIRANHA_TYPE_GREEN_FIRE:
+			if (old_y - y > PIRANHA_GREEN_BBOX_HEIGHT) {
+				y = old_y - PIRANHA_GREEN_BBOX_HEIGHT;
+				SetState(PIRANHA_STATE_ATTACK);
+			}
+			break;
+
+		case PIRANHA_TYPE_RED_FIRE:
+			if (old_y - y > PIRANHA_RED_BBOX_HEIGHT) {
+				y = old_y - PIRANHA_RED_BBOX_HEIGHT;
+				SetState(PIRANHA_STATE_ATTACK);
+			}
+			break;
+		}
+		break;
+
+	case PIRANHA_STATE_DOWN:
+		if (y - old_y >= 0) {
+			y = old_y;
+			SetState(PIRANHA_STATE_IDLE);
+		}
+		break;
+
+	case PIRANHA_STATE_ATTACK:
+	case PIRANHA_STATE_IDLE:
+		if ((GetTickCount64() - time_start) > PIRANHA_ATTACK_IDLE_TIME) {
+			time_start = 0;
+			if (state == PIRANHA_STATE_IDLE) SetState(PIRANHA_STATE_UP);
+			else if (state == PIRANHA_STATE_ATTACK) SetState(PIRANHA_STATE_DOWN);
+		}
+		break;
+	}
+
+	//Get position of Mario
+	if (dynamic_cast<CPlayScene*>(CGame::GetInstance()->GetCurrentScene()))
+		dynamic_cast<CPlayScene*>(CGame::GetInstance()->GetCurrentScene())->GetPlayer()->GetPosition(mario_x, mario_y);
+
 	CGameObject::Update(dt, coObjects);
 }
 
@@ -37,12 +82,39 @@ int CPiranhaPlant::GetAniId()
 		aniId = ID_ANI_PIRANHA_GREEN;
 		break;
 	case PIRANHA_TYPE_GREEN_FIRE:
-		aniId = ID_ANI_PIRANHA_GREEN_FIRE_LEFT;
+		if (mario_x < x) {
+			aniId = ID_ANI_PIRANHA_GREEN_FIRE_LEFT;
+			if (state > PIRANHA_STATE_DOWN) {
+				if (mario_y < y) aniId = ID_ANI_PIRANHA_GREEN_TARGET_TOP_LEFT;
+				else aniId = ID_ANI_PIRANHA_GREEN_TARGET_BOTTOM_LEFT;
+			}
+		}
+		else {
+			aniId = ID_ANI_PIRANHA_GREEN_FIRE_RIGHT;
+			if (state > PIRANHA_STATE_DOWN) {
+				if (mario_y < y) aniId = ID_ANI_PIRANHA_GREEN_TARGET_TOP_RIGHT;
+				else aniId = ID_ANI_PIRANHA_GREEN_TARGET_BOTTOM_RIGHT;
+			}
+		}
 		break;
 	case PIRANHA_TYPE_RED_FIRE:
-		aniId = ID_ANI_PIRANHA_RED_FIRE_LEFT;
+		if (mario_x < x) {
+			aniId = ID_ANI_PIRANHA_RED_FIRE_LEFT;
+			if (state > PIRANHA_STATE_DOWN) {
+				if (mario_y < y) aniId = ID_ANI_PIRANHA_RED_TARGET_TOP_LEFT;
+				else aniId = ID_ANI_PIRANHA_RED_TARGET_BOTTOM_LEFT;
+			}
+		}
+		else {
+			aniId = ID_ANI_PIRANHA_RED_FIRE_RIGHT;
+			if (state > PIRANHA_STATE_DOWN) {
+				if (mario_y < y) aniId = ID_ANI_PIRANHA_RED_TARGET_TOP_RIGHT;
+				else aniId = ID_ANI_PIRANHA_RED_TARGET_BOTTOM_RIGHT;
+			}
+		}
 		break;
 	}
+	DebugOut(L"%d", aniId);
 	return aniId;
 }
 
@@ -50,13 +122,18 @@ void CPiranhaPlant::SetState(int state) {
 	CGameObject::SetState(state);
 	switch (state) {
 	case PIRANHA_STATE_UP:
-		ay = 0;
+		vy = -PIRANHA_UP_DOWN_SPEED;
 		break;
 	case PIRANHA_STATE_DOWN:
-		ay = 0;
+		vy = PIRANHA_UP_DOWN_SPEED;
 		break;
 	case PIRANHA_STATE_ATTACK:
-		ay = 0;
+		time_start = GetTickCount64();
+		vy = 0;
+		break;
+	case PIRANHA_STATE_IDLE:
+		time_start = GetTickCount64();
+		vy = 0;
 		break;
 	}
 }
--- SE102_SuperMarioBros3/PiranhaPlant.h
+++ SE102_SuperMarioBros3/PiranhaPlant.h
@@ -2,39 +2,57 @@
 
 #include "GameObject.h"
 
-#define PIRANHA_UP_DOWN_ACCEL				0.001f
+#define PIRANHA_UP_DOWN_SPEED						0.01f
 
-#define PIRANHA_TYPE_GREEN					1
-#define PIRANHA_TYPE_GREEN_FIRE				2
-#define PIRANHA_TYPE_RED_FIRE				3
+#define PIRANHA_TYPE_GREEN							1
+#define PIRANHA_TYPE_GREEN_FIRE						2
+#define PIRANHA_TYPE_RED_FIRE						3
 
-#define PIRANHA_STATE_UP					111
-#define PIRANHA_STATE_DOWN					222
-#define PIRANHA_STATE_ATTACK				333
+#define PIRANHA_STATE_UP							111
+#define PIRANHA_STATE_DOWN							222
+#define PIRANHA_STATE_ATTACK						333
+#define PIRANHA_STATE_IDLE							444
 
-#define PIRANHA_GREEN_BBOX_WIDTH			15
-#define PIRANHA_GREEN_BBOX_HEIGHT			23
+#define PIRANHA_GREEN_BBOX_WIDTH					15
+#define PIRANHA_GREEN_BBOX_HEIGHT					23
 
-#define PIRANHA_RED_BBOX_WIDTH				15
-#define PIRANHA_RED_BBOX_HEIGHT				31
+#define PIRANHA_RED_BBOX_WIDTH						15
+#define PIRANHA_RED_BBOX_HEIGHT						31
 
-#define ID_ANI_PIRANHA_GREEN				7000
+#define ID_ANI_PIRANHA_GREEN						7000
 
-#define ID_ANI_PIRANHA_GREEN_FIRE_LEFT		7001
-#define ID_ANI_PIRANHA_GREEN_FIRE_RIGHT		7011
+#define ID_ANI_PIRANHA_GREEN_FIRE_LEFT				7001
+#define ID_ANI_PIRANHA_GREEN_TARGET_TOP_LEFT		7041
+#define ID_ANI_PIRANHA_GREEN_TARGET_BOTTOM_LEFT		7021
 
-#define ID_ANI_PIRANHA_RED_FIRE_LEFT		7002
-#define ID_ANI_PIRANHA_RED_FIRE_RIGHT		7012
+#define ID_ANI_PIRANHA_GREEN_FIRE_RIGHT				7011
+#define ID_ANI_PIRANHA_GREEN_TARGET_TOP_RIGHT		7051
+#define ID_ANI_PIRANHA_GREEN_TARGET_BOTTOM_RIGHT	7031
+
+#define ID_ANI_PIRANHA_RED_FIRE_LEFT				7002
+#define ID_ANI_PIRANHA_RED_TARGET_TOP_LEFT			7042
+#define ID_ANI_PIRANHA_RED_TARGET_BOTTOM_LEFT		7022
+
+#define ID_ANI_PIRANHA_RED_FIRE_RIGHT				7012
+#define ID_ANI_PIRANHA_RED_TARGET_TOP_RIGHT			7052
+#define ID_ANI_PIRANHA_RED_TARGET_BOTTOM_RIGHT		7032
+
+#define PIRANHA_ATTACK_IDLE_TIME					5000
 
 class CPiranhaPlant : public CGameObject {
 protected:
 	int ay;
 	int type;
 
+	float old_y;
+	float mario_x, mario_y;
+
+	ULONGLONG time_start;
+
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Render();
 
-	virtual int IsCollidable() { return 0; }
+	virtual int IsCollidable() { return 1; }
 	virtual int IsBlocking() { return 0; }
 
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
@@ -46,9 +64,8 @@ protected:
 public:
 	CPiranhaPlant(float x, float y, int type) :CGameObject(x, y) {
 		this->type = type;
-		vy = 0;
-		vx = 0;
-		ay = 0;
+		old_y = y;
+		time_start = mario_x = mario_y = vy = vx = ay = 0;
 		SetState(PIRANHA_STATE_UP);
 	}
 
