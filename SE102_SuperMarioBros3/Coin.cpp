a089f0733810d7eb221ab70265505ca4c2bcc78a
pahopu <20520278@gm.uit.edu.vn>
2023-06-11 10:43:04 +0700

    Update coin

--- SE102_SuperMarioBros3/Coin.cpp
+++ SE102_SuperMarioBros3/Coin.cpp
@@ -8,10 +8,28 @@ void CCoin::Render()
 	//RenderBoundingBox();
 }
 
+void CCoin::Update(DWORD dt) {
+	if (vy == 0 && ay == 0)
+		return;
+
+	if (ay != 0 && vy > 0)
+		Delete();
+
+	if (ay != 0) {
+		vy += ay * dt;
+		y += vy * dt;
+	}
+}
+
 void CCoin::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x - COIN_BBOX_WIDTH / 2;
 	t = y - COIN_BBOX_HEIGHT / 2;
 	r = l + COIN_BBOX_WIDTH;
 	b = t + COIN_BBOX_HEIGHT;
+}
+
+void CCoin::Deflected(int direction) {
+	ay = COIN_GRAVITY;
+	vy = -COIN_DEFLECT;
 }
--- SE102_SuperMarioBros3/Coin.h
+++ SE102_SuperMarioBros3/Coin.h
@@ -4,17 +4,23 @@
 #include "Animation.h"
 #include "Animations.h"
 
-#define ID_ANI_COIN 11000
+#define ID_ANI_COIN					11000
 
-#define	COIN_WIDTH 10
-#define COIN_BBOX_WIDTH 10
-#define COIN_BBOX_HEIGHT 16
+#define COIN_DEFLECT				0.4f
+#define COIN_GRAVITY				0.001f
+
+//#define	COIN_WIDTH					10
+#define COIN_BBOX_WIDTH				15
+#define COIN_BBOX_HEIGHT			16
 
 class CCoin : public CGameObject {
+	float ay;
+
 public:
-	CCoin(float x, float y) : CGameObject(x, y) {}
+	CCoin(float x, float y) : CGameObject(x, y) { ay = 0; vy = 0; }
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 0; }
+	void Deflected(int direction);
 };
