afb62e79cc43f2b7e5da3c4989fc9748d5bb613f
pahopu <20520278@gm.uit.edu.vn>
2023-06-16 08:42:48 +0700

    Change textures

--- SE102_SuperMarioBros3/Mushroom.h
+++ SE102_SuperMarioBros3/Mushroom.h
@@ -48,7 +48,7 @@ protected:
 	virtual void OnColisionWith(LPCOLLISIONEVENT e);
 
 	void IsDiversion();
-	virtual void Deflected(int direction = 0);
+	virtual void Deflected(int Direction = 0);
 
 public:
 	CMushroom(float x, float y, int type);
