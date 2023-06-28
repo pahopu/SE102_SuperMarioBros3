0d7c0a4fef317e0cc7be180245491910663bd5d2
pahopu <20520278@gm.uit.edu.vn>
2023-06-28 09:25:54 +0700

    Update sprites and class of HUD

--- SE102_SuperMarioBros3/Hud.h
+++ SE102_SuperMarioBros3/Hud.h
@@ -1,8 +1,10 @@
 #pragma once
+#include "Mario.h"
 #include "GameObject.h"
 
 #define ID_ANI_P_CHANGING_COLOR					8888
 
+#define SPEED_LEVEL								0.016f
 #define EFFECT_SPEED							0.02f
 
 #define HUD_BBOX_WIDTH							153
@@ -16,6 +18,9 @@
 
 #define MARIO_SYMBOL_WIDTH						15
 #define MARIO_SYMBOL_HEIGHT						7
+
+#define ARROW_SYMBOL_WIDTH						7
+#define POWER_SYMBOL_WIDTH						14
 
 #define ROW_ABOVE_Y								8	// contain worldmap name, P status and money
 #define ROW_BELOW_Y								16	// contain character, life, score and time
@@ -32,6 +37,11 @@
 
 #define ADJUST									3
 
+#define SCORE_SUPER_MUSHROOM_LEAF				1000
+#define SCORE_COIN								100
+#define SCORE_MARIO_JUMP_ON_ENEMIES				100
+#define SCORE_TAIL_ATTACKED_ENEMIES				100
+
 class CHud : public CGameObject {
 protected:
 	int coin;
@@ -39,6 +49,8 @@ protected:
 	int score;
 
 	vector<int> gifts;
+
+	static CHud* _instance;
 
 	// Parameter length use in case option = 1
 	// Option 0: Normal draw (example: 100 -> draw 100)
@@ -51,6 +63,12 @@ public:
 		coin = 0;
 		life = 3;
 		score = 0;
+	}
+
+	static CHud* GetInstance() {
+		if (_instance == NULL)
+			_instance = new CHud(0, 0);
+		return _instance;
 	}
 
 	void GetBoundingBox(float& left, float& top, float& right, float& bottom);
@@ -67,6 +85,7 @@ public:
 
 	void CollectCoin() { 
 		coin++;
+		score += SCORE_COIN;
 	}
 
 	void Collect1UpMushroom() {
@@ -77,8 +96,12 @@ public:
 		life--; 
 	}
 
-	int AddScore(int Score) { 
-		score = Score; 
+	void CollectScore(int Score) { 
+		score += Score;
+	}
+
+	void CollectGift(int gift) { 
+		gifts.push_back(gift); 
 	}
 
 	int CanRevive() {
