24f5990af8352f6a9781e227592c38943fba18c1
pahopu <20520278@gm.uit.edu.vn>
2023-07-03 14:42:33 +0700

    Add key to handle Mario in world map

--- SE102_SuperMarioBros3/WorldmapKeyEventHandler.cpp
+++ SE102_SuperMarioBros3/WorldmapKeyEventHandler.cpp
@@ -1,5 +1,50 @@
 #include "Game.h"
 #include "debug.h"
-#include "Mario.h"
+#include "MarioWorldmap.h"
 #include "WorldmapScene.h"
 #include "WorldmapKeyEventHandler.h"
+
+
+void CWorldmapKeyEventHandler::OnKeyDown(int KeyCode)
+{
+	//DebugOut(L"[INFO] KeyDown: %d\n", KeyCode);
+	CMarioWorldmap* mario = (CMarioWorldmap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	LPGAME game = CGame::GetInstance();
+	switch (KeyCode)
+	{
+	case DIK_S:
+		mario->SetCanGoIntoPortal(1);
+		break;
+	}
+
+}
+
+void CWorldmapKeyEventHandler::OnKeyUp(int KeyCode)
+{
+	//DebugOut(L"[INFO] KeyUp: %d\n", KeyCode);
+
+	CMarioWorldmap* mario = (CMarioWorldmap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	switch (KeyCode)
+	{
+	case DIK_S:
+		mario->SetCanGoIntoPortal(0);
+		break;
+	}
+}
+
+void CWorldmapKeyEventHandler::KeyState(BYTE* states)
+{
+	CMarioWorldmap* mario = (CMarioWorldmap*)((LPWORLDMAPSCENE)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	LPGAME game = CGame::GetInstance();
+
+	if (mario->IsMoving()) return;
+
+	if (game->IsKeyDown(DIK_RIGHT))
+		mario->SetState(MARIO_WORLDMAP_STATE_WALKING_RIGHT);
+	else if (game->IsKeyDown(DIK_LEFT))
+		mario->SetState(MARIO_WORLDMAP_STATE_WALKING_LEFT);
+	else if (game->IsKeyDown(DIK_UP))
+		mario->SetState(MARIO_WORLDMAP_STATE_WALKING_UP);
+	else if (game->IsKeyDown(DIK_DOWN))
+		mario->SetState(MARIO_WORLDMAP_STATE_WALKING_DOWN);
+}
